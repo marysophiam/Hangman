eaa0dd159d7e4513613711d442399b26b93066d2
Mary Mealiffe <marysophiamealiffe@gmail.com>
2017-02-28 16:10:56 -0800

    replace_blanks_with_letter now working properly

--- hangman_blanks_function.py
+++ hangman_blanks_function.py
@@ -1,63 +1,35 @@
+# Version worked out w/ a student from the Tu/Th class during code brunch
+# on 2/27 (Swati).
+# current_output now resides in main() function; value will now be saved
+# because the variable is in the global scope.
+
 import random
 
-game_word = 'arkansas'    # just a test word for now; good b/c of multiples
-
-# ---------------------------------------------------------------------------
-
-# This works the way I want it to, but the problem is that the guess is still
-# coming from inside the function, not from the have_player_guess_letter
-# function.
-
-def replace_blanks_with_correct_letters():
-
-    current_output = len(game_word) * ['_']
-
-    while True:
-
-        guess = raw_input('Guess a letter: ')
-
-        for index, letter in enumerate(game_word):
-            if letter == guess:
-                current_output[index] = letter
-        print ''.join(current_output)
-
-
-replace_blanks_with_correct_letters()
-
-
-# ---------------------------------------------------------------------------
-
-# In this version, the user is prompted for a guess by passing a returned
-# value in from the have_player_guess_letter function in the main() function
-# (therefore avoiding having the guess come from inside the function).
-# It works, but the problem is that each time a new letter is guessed, the
-# current_output variable is reset to all '_'s instead of the value
-# being permanently changed. When I messed around with putting while True:
-# inside this function, I ended up in an endless loop after the first guess
-# was made.
+game_word = "arkansas"
 
 def have_player_guess_letter():
 
-    player_guess = raw_input('Guess a letter: ')
+    player_guess = raw_input("Guess a letter: ")
     return player_guess
 
 
-def replace_blanks_with_correct_letters(guess):
-
-    current_output = len(game_word) * ['_']
+def replace_blanks_with_correct_letter(current_output, guess):
 
     for (index, letter) in enumerate(game_word):
         if letter == guess:
             current_output[index] = letter
-    print ''.join(current_output)
-    return current_output
+    print "".join(current_output)
 
 
 def main():
 
+    current_output = len(game_word) * ["_"]
+
     while True:
 
-        replace_blanks_with_correct_letters(have_player_guess_letter())
-
+        replace_blanks_with_correct_letter(current_output, have_player_guess_letter())
 
 main()
+
+# The new challenge is to pass in a game_word from a function choosing that word
+# (get_random_word). Having issues with that right now...working on it!
