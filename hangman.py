e1404e9dad3d735cbed38519133b860ddc73add3
Mary Mealiffe <marysophiamealiffe@gmail.com>
2017-02-28 17:04:58 -0800

    possible solution to passing in game_word issue

--- hangman.py
+++ hangman.py
@@ -1,10 +1,16 @@
 import random
 
-game_word = "arkansas"
+def get_random_word(word_list):
+    # For finished game, import .txt file(s) for word choice
+
+    global game_word
+    game_word = random.choice(word_list)
+    return game_word
+
 
 def have_player_guess_letter():
 
-    player_guess = raw_input("Guess a letter: ")
+    player_guess = raw_input('Guess a letter: ')
     return player_guess
 
 
@@ -13,12 +19,16 @@ def replace_blanks_with_correct_letter(current_output, guess):
     for (index, letter) in enumerate(game_word):
         if letter == guess:
             current_output[index] = letter
-    print "".join(current_output)
+    print ''.join(current_output)
 
 
 def main():
 
-    current_output = len(game_word) * ["_"]
+    word_list = ['apple', 'berry', 'cherry', 'arkansas', 'mississippi', 'hawaii', 'serendipity', 'insomnia']    # sample list--good for testing
+
+    game_word = get_random_word(word_list)
+
+    current_output = len(game_word) * ['_']
 
     while True:
 
