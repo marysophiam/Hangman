03adc203579d51efe12ec23bd5d8b2788bb4969c
Mary Mealiffe <marysophiamealiffe@gmail.com>
2017-03-02 01:01:36 -0800

    get_random_word() now passing correctly into main()

--- hangman.py
+++ hangman.py
@@ -1,11 +1,12 @@
 import random
 
-def get_random_word(word_list):
-    # For finished game, import .txt file(s) for word choice
+# For finished game, import .txt file(s) for word choice
+word_list = ['apple', 'berry', 'cherry', 'arkansas', 'mississippi', 'hawaii', 'serendipity', 'insomnia']    # sample list--good for testing
 
-    global game_word
-    game_word = random.choice(word_list)
-    return game_word
+def get_random_word():
+
+    random_word = random.choice(word_list)
+    return random_word
 
 
 def have_player_guess_letter():
@@ -14,7 +15,7 @@ def have_player_guess_letter():
     return player_guess
 
 
-def replace_blanks_with_correct_letter(current_output, guess):
+def replace_blanks_with_correct_letter(current_output, guess, game_word):
 
     for (index, letter) in enumerate(game_word):
         if letter == guess:
@@ -24,15 +25,12 @@ def replace_blanks_with_correct_letter(current_output, guess):
 
 def main():
 
-    word_list = ['apple', 'berry', 'cherry', 'arkansas', 'mississippi', 'hawaii', 'serendipity', 'insomnia']    # sample list--good for testing
-
-    game_word = get_random_word(word_list)
-
+    game_word = get_random_word()
     current_output = len(game_word) * ['_']
 
     while True:
 
-        replace_blanks_with_correct_letter(current_output, have_player_guess_letter())
+        replace_blanks_with_correct_letter(current_output, have_player_guess_letter(), game_word)
 
 
 if __name__ == "__main__":
